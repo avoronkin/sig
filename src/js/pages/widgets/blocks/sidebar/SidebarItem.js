d37967d52018aed2602888a8bee0aefb3e2418db
Alexandr Voronkin <admin@elasticweb.ru>
2014-03-04 15:14:29 +0400

    [wip]

--- src/js/pages/widgets/blocks/sidebar/SidebarItem.js
+++ src/js/pages/widgets/blocks/sidebar/SidebarItem.js
@@ -2,6 +2,7 @@ var View = require('../../../../core/views/View');
 var template = require('./sidebar-item.html');
 var $ = require('jquery');
 require('jquery.event.drag');
+var _ = require('underscore');
 
 module.exports = View.extend({
     initialize: function () {
@@ -17,23 +18,50 @@ module.exports = View.extend({
     afterRender: function () {
         var view = this;
 
-        view.$el.drag("start", function () {
+        view.$el.drag("start", function (ev, dd) {
+            console.log('drag start', arguments);
             view.clone = view.$el.clone().css({
                 position: 'absolute',
                 opacity: 0.75,
                 width: view.$el.width(),
                 height: view.$el.height()
             });
+            
             return view.clone.appendTo(document.body);
+            // return gridster.add_widget('<li>ewyewueiwue</li>').css({
+            //     top: dd.offsetY,
+            //     left: dd.offsetX
+            // });
+
         }).drag(function (ev, dd) {
-            $(dd.proxy).css({
-                top: dd.offsetY,
-                left: dd.offsetX
-            });
+            if(dd.drop.length){
+                console.log('drag', dd.drop[0])
+                // $(dd.proxy).appendTo($(dd.drop[0])) 
+                _.once(function(){
+                    console.log('once')
+                    dd.proxy = gridster.add_widget($(dd.proxy)).trigger('mouseup').removeAttr('style').css({
+                        width: 320,
+                        height: 185
+                    },3,3,1,1)
+                    dd.proxy = gridster.resize_widget(dd.proxy, 3,3);
+                })();
+
+            }else{
+                $(dd.proxy).css({
+                    top: dd.offsetY,
+                    left: dd.offsetX
+                });
+            
+            }
         },{
             // drop: '.node-drop' 
         }).drag("end", function (ev, dd) {
-            $(dd.proxy).remove();
+
+            if(dd.drop.length){
+
+            }else{
+                $(dd.proxy).remove();
+            }
         });
 
     },
--- src/js/pages/widgets/blocks/widgets/Widgets.js
+++ src/js/pages/widgets/blocks/widgets/Widgets.js
@@ -1,7 +1,7 @@
 var View = require('../../../../core/views/View');
 var template = require('./widgets.html');
 var $ = require('jquery');
-// require('jquery.event.drag');
+require('jquery.event.drop');
 require('jquery.gridster');
 
 module.exports = View.extend({
@@ -13,28 +13,42 @@ module.exports = View.extend({
     //     return this.model.toJSON();
     // },
     afterRender: function () {
+        this.$el.find('ul').drop(function (ev, dd) {
+            console.log('drop', ev, dd);
+        }).drop("start", function (ev, dd) {
+            // var type = $(dd.drag).data('type');
+            // if (type === 'tree') {
+                $(this).addClass("drop-active");
+            // }
+            console.log('start', arguments);
+        }).drop("end", function () {
+            $(this).removeClass("drop-active");
+        });
 
-        var gridster = $(".gridster > ul").gridster({
+        gridster = $(".gridster > ul").gridster({
             widget_margins: [5, 5],
             widget_base_dimensions: [100, 55],
+            max_cols: 9,
+            min_cols: 9,
+            // min_rows:3,
+            helper: 'clone',
             resize: {
-                enable: true 
+                enabled: true 
             }
         }).data('gridster');
 
-        console.log('w after render ', gridster)
         var widgets = [
-            ['<li>0</li>', 1, 2],
-            ['<li>1</li>', 3, 2],
-            ['<li>2</li>', 3, 2],
-            ['<li>3</li>', 2, 1],
-            ['<li>4</li>', 4, 1],
-            ['<li>5</li>', 1, 2],
-            ['<li>6</li>', 2, 1],
-            ['<li>7</li>', 3, 2],
-            ['<li>8</li>', 1, 1],
-            ['<li>9</li>', 2, 2],
-            ['<li>10</li>', 1, 3]
+            ['<li>0</li>', 3, 3],
+        //     ['<li>1</li>', 3, 2],
+        //     ['<li>2</li>', 3, 2],
+        //     ['<li>3</li>', 2, 1],
+        //     ['<li>4</li>', 4, 1],
+        //     ['<li>5</li>', 1, 2],
+        //     ['<li>6</li>', 2, 1],
+        //     ['<li>7</li>', 3, 2],
+        //     ['<li>8</li>', 1, 1],
+        //     ['<li>9</li>', 2, 2],
+        //     ['<li>10</li>', 1, 3]
         ];
 
         $.each(widgets, function (i, widget) {
